e0c0a6e77e846d0ca7567a8a47e89833a13119e8
TanHaoYou <111227874+TanHaoYou@users.noreply.github.com>
2022-08-30 10:45:48 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.c0actdelt4pf.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "tanhaoyou-employee"
+customregion = "us-east-1"
